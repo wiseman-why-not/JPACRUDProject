67c9d1e71aea8ce1671f981e333cfe6c32d096c1
wiseman-why-not <marcus.e.wiseman@gmail.com>
2020-01-17 15:14:19 -0700

    Created Boot MVC project, link JPA dependency

---
+++ ThunderRUs/src/main/java/com/skilldistillery/Thunder/ThunderRUsApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.Thunder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThunderRUsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThunderRUsApplication.class, args);
+	}
+
+}
---
+++ ThunderRUs/src/test/java/com/skilldistillery/Thunder/ThunderRUsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.Thunder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThunderRUsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
