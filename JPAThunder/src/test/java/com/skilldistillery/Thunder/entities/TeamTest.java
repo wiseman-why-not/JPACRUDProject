ffa9639074ea6b14c8818380c32f28e435a419d1
wiseman-why-not <marcus.e.wiseman@gmail.com>
2020-01-17 18:39:08 -0700

    tested all team class getters and setters, all working

--- JPAThunder/src/test/java/com/skilldistillery/Thunder/entities/TeamTest.java
+++ JPAThunder/src/test/java/com/skilldistillery/Thunder/entities/TeamTest.java
@@ -43,7 +43,135 @@ class TeamTest {
 	}
 
 	@Test
+	@DisplayName("testing get team field goal attempt method")
 	void test1() {
+		assertEquals(85.3, team.getFieldGoalAttempt());
+	}
+	
+	@Test
+	@DisplayName("testing get team ield goal made method")
+	void test2() {
+		assertEquals(39.9, team.getFieldGoalMade());
+	}
+	
+	@Test
+	@DisplayName("testing get team ield goal percentage method")
+	void test3() {
+		assertEquals(46.70, team.getFieldGoalPercentage());
+	}
+	
+	@Test
+	@DisplayName("testing get team name method")
+	void test4() {
+		assertEquals(30.1, team.getThreePointAttempt());
+	}
+	
+	@Test
+	@DisplayName("testing get team name method")
+	void test5() {
+		assertEquals(10.3, team.getThreePointMade());
+	}
+	
+	@Test
+	@DisplayName("testing get team name method")
+	void test6() {
+		assertEquals(34.30, team.getThreePointPercentage());
+	}
+	
+	@Test
+	@DisplayName("testing get team name method")
+	void test7() {
+		assertEquals(24.3, team.getFreeThrowAttempt());
+	}
+	
+	@Test
+	@DisplayName("testing get team name method")
+	void test8() {
+		assertEquals(19.4, team.getFreeThrowMade());
+	}
+	
+
+	
+	@Test
+	@DisplayName("testing get team name method")
+	void test9() {
+		assertEquals(79.80, team.getFreeThrowPercentage());
+	}
+	
+	@Test
+	@DisplayName("testing get team name method")
+	void test10() {
+		assertEquals(43.4, team.getReboundsAverage());
+	}
+	
+	@Test
+	@DisplayName("testing get team offensive rebound method")
+	void test11() {
+		assertEquals(8.5, team.getOffensiveRebound());
+	}
+	
+	@Test
+	@DisplayName("testing get team defensive rebound method")
+	void test12() {
+		assertEquals(34.9, team.getDefensiveRebound());
+	}
+	
+	@Test
+	@DisplayName("testing get team assists method")
+	void test13() {
+		assertEquals(21.7, team.getAssists());
+	}
+	
+	@Test
+	@DisplayName("testing get team turn overs method")
+	void test14() {
+		assertEquals(14, team.getTurnOvers());
+	}
+	
+	@Test
+	@DisplayName("testing get team blocks method")
+	void test15() {
+		assertEquals(4.8 , team.getBlocks());
+	}
+	
+	@Test
+	@DisplayName("testing get team personal foulds method")
+	void test16() {
+		assertEquals(18.8 , team.getPersonalFouls());
+	}
+	
+	@Test
+	@DisplayName("testing get team name method")
+	void test17() {
+		assertEquals(.56, team.getWinLossPercentage());
+	}
+	
+	@Test
+	@DisplayName("testing get team name method")
+	void test18() {
+		assertEquals(23, team.getWins());
+	}
+	
+	@Test
+	@DisplayName("testing get team name method")
+	void test19() {
+		assertEquals(18, team.getLosses());
+	}
+	@Test
+	@DisplayName("testing get team name method")
+	void test20() {
+		assertEquals(109.4, team.getPointsScoredAverage());
+	}
+	
+	@Test
+	@DisplayName("testing get team name method")
+	void test21() {
+		assertEquals(41, team.getGamesPlayed());
+	}
+	
+	@Test
+	@DisplayName("testing get team name method")
+	void test22() {
 		assertEquals("Oklahoma City Thunder", team.getTeamName());
 	}
 
